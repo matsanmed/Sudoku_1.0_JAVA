8b71b5a6393cca1e42e211f75093914b37abb7f1
Matheus Santos de Medeiros <matsanmed@gmail.com>
2016-09-24 19:07:57 -0300

    1.1.1

--- src/sudoku_ed_mi/Sudo.java
+++ src/sudoku_ed_mi/Sudo.java
@@ -2,6 +2,7 @@ package sudoku_ed_mi;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class Sudo {
 
@@ -115,6 +116,19 @@ public class Sudo {
         }
 }
      
+    void zerarDados() {
+        for (int l = 0; l < 9; l++) {
+            for (int c = 0; c < 9; c++) {
+                mGabarito[l][c] = 0;
+            }
+        }
+        liq = 0;	// linha inicial do quadrado
+        ciq = 0;	// coluna inicial do quadrado
+        lfq = 0;	// linha final do quadrado
+        cfq = 0;	// coluna final do quadrado
+        linha = 0;	// linha de trabalho -> insercao de numeros
+        coluna = 0;	// coluna de trabalho -> insercao de numeros
+    }
  
     void alocarNumeros() {
         int quadrado;
@@ -122,6 +136,7 @@ public class Sudo {
         int voltar = 0;
         int ultimoQuadradoRepetido = 0;
         boolean vrl, vrc;
+        zerarDados();
         for (quadrado = 1; quadrado < 10; quadrado++) {
             acharLimitesQuadrado(quadrado);
             for (int n = 1; n < 10; n++) {
@@ -181,11 +196,10 @@ public class Sudo {
                    "\n\n " + mGabarito[6][0] + " " + mGabarito[6][1] + " " + mGabarito[6][2] + "     " + mGabarito[6][3] + " " + mGabarito[6][4] + " " + mGabarito[6][5] + "     " + mGabarito[6][6] + " " + mGabarito[6][7] + " " + mGabarito[6][8]+
                      "\n " + mGabarito[7][0] + " " + mGabarito[7][1] + " " + mGabarito[7][2] + "     " + mGabarito[7][3] + " " + mGabarito[7][4] + " " + mGabarito[7][5] + "     " + mGabarito[7][6] + " " + mGabarito[7][7] + " " + mGabarito[7][8]+
                      "\n " + mGabarito[8][0] + " " + mGabarito[8][1] + " " + mGabarito[8][2] + "     " + mGabarito[8][3] + " " + mGabarito[8][4] + " " + mGabarito[8][5] + "     " + mGabarito[8][6] + " " + mGabarito[8][7] + " " + mGabarito[8][8], "a batalha acabou", 0);
-                     
-        
+              
+
+
+
+
     }
-
-
-
-
 }
